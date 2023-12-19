f57ce27d1bc1e58e625300314ffa3a63fc1371e2
hardik <hardik.abril@gmail.com>
2023-12-19 14:58:04 +0530

    set Navigation drawer

--- app/src/main/java/com/hardik/videoplayerbase/MainActivity.kt
+++ app/src/main/java/com/hardik/videoplayerbase/MainActivity.kt
@@ -4,14 +4,18 @@ import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.hardik.videoplayerbase.databinding.ActivityMainBinding
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -19,6 +23,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         requestRuntimePermission()
+        //for NavDrawer
+        toggle = ActionBarDrawerToggle(this,binding.root,R.string.open,R.string.close)
+        binding.root.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         setFragment(VideosFragment())
         binding.bottomNav.setOnItemSelectedListener {
             when(it.itemId){
@@ -26,6 +36,17 @@ class MainActivity : AppCompatActivity() {
                 R.id.folders_view -> setFragment(FoldersFragment())
             }
             return@setOnItemSelectedListener true
+        }
+        binding.navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.feedbackNav -> Toast.makeText(this,"Feedback",Toast.LENGTH_SHORT).show()
+                R.id.themesNav -> Toast.makeText(this,"Theme",Toast.LENGTH_SHORT).show()
+                R.id.sortOrderNav -> Toast.makeText(this,"Sort Order",Toast.LENGTH_SHORT).show()
+                R.id.aboutNav -> Toast.makeText(this,"About",Toast.LENGTH_SHORT).show()
+                R.id.exitNav -> exitProcess(1)//close app, parameter value doesn't matter
+
+            }
+            return@setNavigationItemSelectedListener true
         }
     }
     private fun setFragment(fragment: Fragment){
@@ -54,5 +75,11 @@ class MainActivity : AppCompatActivity() {
             else
                 ActivityCompat.requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE),13)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+            return true
+        return super.onOptionsItemSelected(item)
     }
 }
